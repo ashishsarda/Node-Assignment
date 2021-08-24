c80d303606796d8b3f8150117244a85869878ed3
Ashish Sarda <sardaashish16@gmail.com>
2021-08-24 14:01:30 +0530

    added log files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express               =  require('express'),
-      app                   =  express(),
-      mongoose              =  require("mongoose"),
-      passport              =  require("passport"),
-      bodyParser            =  require("body-parser"),
-      LocalStrategy         =  require("passport-local"),
-      passportLocalMongoose =  require("passport-local-mongoose"),
-      User                  =  require("./models/user");
+const express =  require('express'),
+      app =  express(),
+      mongoose =  require("mongoose"),//Using mongoDB as the database
+      passport =  require("passport"),//Using passport package for suer authentication
+      bodyParser =  require("body-parser"),//TO parse the incoming data
+      LocalStrategy =  require("passport-local"),//Passport Strategy used by Passport to verify users
+      logger = require('./logger'),//Logger function to log information
+      User  =  require("./models/user");//User module 
 
 
 //Connecting database
@@ -22,40 +22,52 @@ app.use(require("express-session")({
 
 passport.serializeUser(User.serializeUser());       //session encoding
 passport.deserializeUser(User.deserializeUser());   //session decoding
-passport.use(new LocalStrategy(function(username, password, done) { //Using the Passport Local Strategy to authenticate Users
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }))
+passport.use(new LocalStrategy(User.authenticate()));
+// passport.use(new LocalStrategy(function(username, password, done) { //Using the Passport Local Strategy to authenticate Users
+//     User.findOne({ username: username }, function (err, user) {
+//         passport.use(new LocalStrategy(User.authenticate()))
+//     }})));
+    //   if (err) { return done(err); }
+    //   if (!user) {
+    //     return done(null, false, { message: 'Incorrect username.' });
+    //   }
+    //   if (!user.validPassword(password)) {
+    //     return done(null, false, { message: 'Incorrect password.' });
+    //   }
+    //   return done(null, user);
+    // });
+
 app.set("view engine","ejs");// Setting EJS as the default templating engine
 app.use(bodyParser.urlencoded(
       { extended:true }
 ))
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 //Home Page
 app.get("/", (req,res) =>{
+    logger.info('accessed the home page')
     res.render("home");
 })
+
 //Display the currently logged in user if he is authenticated using a middleware.
 app.get("/user",isLoggedIn ,(req,res) =>{
     try{
+        logger.info('accessed the user profile page')
         res.render("user");
     }catch(e){
-        console.log('Please log in')
+        logger.error('Failed to log in')
+        //console.log('Please log in')
     }
 })
+
 //Registering a new User
 app.get("/register",(req,res)=>{
+    logger.info('accessed the register page')
     res.render("register");
 })
+
 app.post("/register",(req,res)=>{
     
     User.register(new User({fname:req.body.fname,lname:req.body.lname,username: req.body.username,phone:req.body.phone,birth: req.body.birth}),req.body.password,function(err,user){
@@ -64,20 +76,23 @@ app.post("/register",(req,res)=>{
             res.render("register");
         }
     passport.authenticate("local")(req,res,function(){
+        logger.info('New User registered')
         res.redirect("/login");
     })    
     })
 })
+
 //Directing to Logging in Users
 app.get("/login",(req,res)=>{
+    logger.info('Trying to login')
     res.render("login");
 })
+
 //Authenticating the details provided and checking if it matches the database
 //If it does user will be redirected to user page or login page
 app.post("/login",passport.authenticate("local",{
     successRedirect:"/user",
-    failureRedirect:"/login",
-    failureFlash:true
+    failureRedirect:"/login"
 }),function (req, res){
 
 })
@@ -85,12 +100,14 @@ app.post("/login",passport.authenticate("local",{
 
 // .logout() is provided by the Passport module for easy logout which is attched to request
 app.get("/logout",(req,res)=>{
+    logger.info(`${req.user.fname} has logged out`)
     req.logout();
     res.redirect("/");
 });
 
 //Returns an array of json 
 app.get('/jobs',(req,res)=>{
+    logger.info('Acessed the jobs page')
     res.json({
         "jobs":[
             {"Front End Developer":5},
@@ -101,6 +118,7 @@ app.get('/jobs',(req,res)=>{
 })
 //To serve the meme page and receive an input
 app.get('/meme',(req,res)=>{
+    logger.info('Acessed the meme page')
     res.render('meme')
 })
 //To check if the input string which is attached to the body of the request satisfies a condition.
@@ -117,6 +135,7 @@ app.post('/meme',(req,res)=>{
 
 //Serving the details of the user currently logged in using the isLoggedIn middleware to authenticate if the suer is logged in
 app.get('/intro',isLoggedIn,(req,res)=>{
+    logger.info('Acessed the intro page')
     res.json({
         "message":`string- Hey my name is ${req.user.fname} ${req.user.lname} and I'm a 3rd year student at VIT.`,
         "First Name":req.user.fname,
@@ -130,6 +149,7 @@ function isLoggedIn(req,res,next) {
     if(req.isAuthenticated()){
         return next();
     }
+    logger.error('User not logged in')
     //console.log('Please authenticate')
     res.redirect("/login");
 } 
